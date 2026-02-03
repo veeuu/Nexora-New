5127ee0722cf9b72f87e7b7220c343ff815361d5
Developer <dev@example.com>
2026-02-03 12:34:49 +0530

    fix bugs

--- frontend/src/components/martech/NTP.jsx
+++ frontend/src/components/martech/NTP.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { rowMatchesSearch, highlightText, Tooltip, createTooltipHandlers } from '../../utils/tableUtils';
+import { rowMatchesSearch, highlightText, Tooltip, createTooltipHandlers } from '../../../utils/tableUtils';
 import * as SiIcons from 'react-icons/si';
 
 // Generic Custom Dropdown Component (without icons)
